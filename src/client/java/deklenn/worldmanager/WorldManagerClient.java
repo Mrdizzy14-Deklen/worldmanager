c6933f7040c3bc67211eea961dda2463134e89ab
Deklen Nates <dmn230001@utdallas.edu>
2025-11-23 00:18:47 -0600

    Base files gen

---
+++ src/client/java/deklenn/worldmanager/WorldManagerClient.java
@@ -0,0 +1,10 @@
+package deklenn.worldmanager;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldManagerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
